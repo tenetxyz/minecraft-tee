76c73c68a0f7f81d8ebf7a88fd1392a653e38669
Curtis Chong <curtischong5@gmail.com>
2023-03-21 16:48:40 -0400

    wip dockerize nitriding

--- dockerize-mc-and-nitriding/start.sh
+++ dockerize-mc-and-nitriding/start.sh
@@ -8,5 +8,8 @@ echo "[sh] Started nitriding."
 # if the server fails to spin up in that time, then kill the container
 # otherwise, call the nitriding url at http://127.0.0.1:8080/enclave/ready
 
+# minecraft servers expect things like the eula and server.properties to be in the same dir
+cd server
+
 # spin up the server
 java -Xmx1024M -Xms1024M -jar server-1.18.2.jar nogui &

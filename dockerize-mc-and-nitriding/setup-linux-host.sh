de45890471c13626d6d78200424a5317916c16a0
Curtis Chong <curtischong5@gmail.com>
2023-03-22 00:28:25 -0400

    setup linux host

--- dockerize-mc-and-nitriding/setup-linux-host.sh
+++ dockerize-mc-and-nitriding/setup-linux-host.sh
@@ -12,6 +12,12 @@ git pull --recurse-submodules
 # now build the nitriding code
 cd nitriding
 go get # install dependencies
-go build cmd/main.go
+go build cmd/main.go # the output file is in this nitriding dir
 
-# the output file is in this nitriding dir
+# now install enclave code
+sudo amazon-linux-extras install aws-nitro-enclaves-cli -y
+sudo yum install aws-nitro-enclaves-cli-devel -y
+sudo usermod -aG ne ec2-user
+sudo usermod -aG docker ec2-user
+
+echo "Please log out and re-connect to ensure nitro cli installed correctly"

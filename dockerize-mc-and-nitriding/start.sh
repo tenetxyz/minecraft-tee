66d2955b5bca65423d576bb32e1e479b38ca181e
Curtis Chong <curtischong5@gmail.com>
2023-03-21 20:40:45 -0400

    fix docker file imports

--- dockerize-mc-and-nitriding/start.sh
+++ dockerize-mc-and-nitriding/start.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 #nitriding_url="http://127.0.0.1:8080/enclave/ready"
+echo "[start.sh] Starting"
 /nitriding -fqdn example.com  -extport 8443  -intport 8080 &
 echo "[sh] Started nitriding."
 

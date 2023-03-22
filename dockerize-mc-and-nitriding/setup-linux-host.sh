a2072e308b7192974ce152a0a16475bd5674ccdf
Curtis Chong <curtischong5@gmail.com>
2023-03-22 03:39:32 +0000

    update setup host code

--- dockerize-mc-and-nitriding/setup-linux-host.sh
+++ dockerize-mc-and-nitriding/setup-linux-host.sh
@@ -4,4 +4,11 @@ sudo yum install golang
 
 git submodule update --init --recursive
 git pull --recurse-submodules
-make nitriding/cmd/nitriding
+
+# now build the nitriding code
+cd nitriding
+go get # install dependencies
+go build cmd/main.go
+
+# the output file is in this nitriding dir
+
